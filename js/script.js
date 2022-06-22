36dea57c9e908bed03993305062505c0a8ebd84b
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-06-23 00:17:29 +0530

    about complete

--- js/script.js
+++ js/script.js
@@ -8,3 +8,19 @@ window.onmousemove = (e) =>{
     cursor2.style.top = e.pageY + 'px';
     cursor2.style.left = e.pageX + 'px';
 }
+
+// About Tabs
+const tabsContainer = document.querySelector(".about-tabs"),
+aboutSection = document.querySelector(".about-section");
+
+tabsContainer.addEventListener("click", (e) => {
+    // console.log(e.target);
+    if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")) {
+        tabsContainer.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const target = e.target.getAttribute("data-target");
+        // console.log(target);
+        aboutSection.querySelector(".tab-content.active").classList.remove("active");
+        aboutSection.querySelector(target).classList.add("active");
+    }
+})
