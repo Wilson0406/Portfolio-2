b791e19c293194b93e25b2c3daa4319d3e0e78dd
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-06-22 12:41:19 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+// Custom Cursor
+let cursor1 = document.querySelector('.cursor-1');
+let cursor2 = document.querySelector('.cursor-2');
 
+window.onmousemove = (e) =>{
+    cursor1.style.top = e.pageY + 'px';
+    cursor1.style.left = e.pageX + 'px';
+    cursor2.style.top = e.pageY + 'px';
+    cursor2.style.left = e.pageX + 'px';
+}
