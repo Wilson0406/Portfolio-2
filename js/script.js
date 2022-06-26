2f7c9ccac139659a277a8e517ba881d8025a3ea9
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-06-27 01:35:09 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,3 +24,22 @@ tabsContainer.addEventListener("click", (e) => {
         aboutSection.querySelector(target).classList.add("active");
     }
 })
+
+// Portfolio Item Popup
+
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("view-project-btn")) {
+        // console.log("test");
+        togglePortfolioPopup();
+        portfolioItemDetails();
+        
+    }
+})
+
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio-popup").classList.toggle("open");
+    document.body.classList.toggle("hide-scrolling");
+    document.querySelector(".main").classList.toggle("fade-out");
+}
+
+document.querySelector(".pp-close").addEventListener("click", togglePortfolioPopup);
